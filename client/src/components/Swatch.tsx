38dd2ab7aac0716cf4673f4ad5ddd27b6610320d
newportkat <thomas.katilius@gmail.com>
2023-02-02 17:16:50 +1100

    wrapped delete and save svg in divs

--- client/src/components/Swatch.tsx
+++ client/src/components/Swatch.tsx
@@ -28,38 +28,42 @@ const Swatch = (props: any) => {
     ))
 
     return (
-        <div className="flex flex-wrap">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-12 w-12 cursor-pointer p-1 backdrop-contrast-75 hover:backdrop-contrast-50"
-                onClick={() => exportAsImage(exportRef.current, props.id)}
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
-                />
-            </svg>
-            <div ref={exportRef} className="flex flex-wrap">{swatchElements}</div>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-12 w-12 cursor-pointer p-1 backdrop-contrast-75 hover:backdrop-contrast-50"
-                onClick={props.deletePalette}
-            >
-                <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                />
-            </svg>
+        <div className="flex flex-wrap ">
+            <div className="h-12 w-12 flex-shrink-0 cursor-pointer p-1 backdrop-contrast-75 hover:backdrop-contrast-50">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    onClick={() => exportAsImage(exportRef.current, props.id)}
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                    />
+                </svg>
+            </div>
+            <div ref={exportRef} className="flex flex-wrap">
+                {swatchElements}
+            </div>
+            <div className="h-12 w-12 flex-shrink-0 cursor-pointer p-1 backdrop-contrast-75 hover:backdrop-contrast-50">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    onClick={props.deletePalette}
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                    />
+                </svg>
+            </div>
         </div>
     )
 }
